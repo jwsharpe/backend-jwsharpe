bc9e99b338331105e58eb0d7a014434d9de062a6
James Sharpe <jameswrightsharpe@gmail.com>
2020-05-06 22:36:41 -0400

    updated

--- schema.sql
+++ schema.sql
@@ -2,7 +2,7 @@ CREATE TABLE users(
     id INT PRIMARY KEY,
     username VARCHAR(255), 
     visits INT,
-    created_on TIMESTAMP NOT NULL,
-    updated_on TIMESTAMP
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP
 );
 
